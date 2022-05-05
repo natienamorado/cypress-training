fd6912406839fec6d060feed6b54985365740a95
Natalia <natalia.pereze@udea.edu.co>
2022-05-05 16:57:22 -0500

    corrección

--- cypress/integration/google.spect.js
+++ cypress/integration/google.spect.js
@@ -1,5 +1,5 @@
 describe("This is my first cypress test", () => {
-    it("should have a title", () => {
+    it("should have a title a", () => {
         cy.visit("https://www.google.com/");
         cy.title().should("eq", "Google");
     });
